ec43779c20e2d2c707bc9065806d08e26eda9c53
Sukhman75 <sukhmansingh75@gmail.com>
2019-02-05 23:28:43 -0500

    Got the app credentials

---
+++ Fetching_Data.py
@@ -0,0 +1,8 @@
+import twython
+import json
+
+#open the json file 
+
+with open("C:\\Users\\Sukhman Singh\\Desktop\\Capstone Project\\twitter_credentials.json", "r") as file:
+	credentials = json.load(file)
+
