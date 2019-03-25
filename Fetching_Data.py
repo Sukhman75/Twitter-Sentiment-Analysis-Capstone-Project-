a12daedc597feb41eebccf364ecf2c28d0190dae
Sukhman75 <sukhmansingh75@gmail.com>
2019-03-25 01:02:53 -0400

    Changes Made

--- Fetching_Data.py
+++ Fetching_Data.py
@@ -5,6 +5,7 @@ import csv
 import re
 import pandas as pd
 import nltk
+import numpy as np 
 import textblob
 #open the json file 
 
@@ -87,11 +88,14 @@ df3 = pd.DataFrame({'Senti':POLS})
 
 Bit_Vals =pd.read_csv("Bit_Values.csv")
 #Drop the empty rows if any.
-Bit_Vals = Bit_Vals.dropna(axis = 0, how = 'any')
-
+#Bit_Vals = Bit_Vals.dropna(axis = 0, how = 'any')
+print(Bit_Vals.head())
 df_time = Bit_Vals["Timestamp"]
 BIT_Volume =Bit_Vals["Volume_(BTC)"]
-BIT_Price = Bit_Vals["Weighted_Price"]
+BIT_WtPrice = Bit_Vals["Weighted_Price"]
+BIT_Price = Bit_Vals["Price"]
+BIT_Price = BIT_Price.fillna(BIT_Price.mean())
+
 #print(BIT_Price.head(20))
 
 #Building a Final Data Frame that will Be Used
@@ -112,11 +116,11 @@ Classification_DF = Class_df.dropna(axis = 0, how = 'any')
 
 
 """ DataFrame for Regression Problem """
-Regress_df = pd.concat([df_time,df2,BIT_Volume,BIT_Price], axis=1 )
+Regress_df = pd.concat([df_time,df2,BIT_Volume,BIT_WtPrice,BIT_Price], axis=1 )
 
 
-Regress_DF = Regress_df.dropna(axis = 0, how = 'any')
-#print(Regress_DF.tail(10))
+Regress_df = Regress_df.dropna(axis = 0, how = 'any')
+#print(Regress_df.tail(10))
 
 
 
@@ -135,49 +139,49 @@ from sklearn.model_selection import train_test_split
 import sklearn.model_selection
 from sklearn import preprocessing, metrics, svm, tree, ensemble
 
-X = Classification_DF.drop(["Senti"], axis =1)
-print(X.head())
-#X_F = X.iloc[:, 0:4].values 
-#print(X_F.head())
-Y = Classification_DF["Senti"]
-#Y = Classification_DF.iloc[:,4].values  
-#print(Y.head())
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state = 0)
+# X = Classification_DF.drop(["Senti"], axis =1)
+# print(X.head())
+# #X_F = X.iloc[:, 0:4].values 
+# #print(X_F.head())
+# Y = Classification_DF["Senti"]
+# #Y = Classification_DF.iloc[:,4].values  
+# #print(Y.head())
+# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state = 0)
 
-pipeline = Pipeline([('scaler', StandardScaler()), ('SVM', SVC())])
+# pipeline = Pipeline([('scaler', StandardScaler()), ('SVM', SVC())])
 
-pipeline.fit(X_train,y_train)
+# pipeline.fit(X_train,y_train)
 
-Predictions = pipeline.predict(X_test)
+# Predictions = pipeline.predict(X_test)
 
-c_m = metrics.confusion_matrix(y_test, Predictions)
-c_r = metrics.classification_report(y_test, Predictions)
-m_a_e = metrics.mean_absolute_error(y_test,Predictions)
+# c_m = metrics.confusion_matrix(y_test, Predictions)
+# c_r = metrics.classification_report(y_test, Predictions)
+# m_a_e = metrics.mean_absolute_error(y_test,Predictions)
 
-print(c_m,'\n', c_r,'\n', m_a_e)
-print(metrics.accuracy_score(y_test, Predictions))
+# print(c_m,'\n', c_r,'\n', m_a_e)
+# print(metrics.accuracy_score(y_test, Predictions))
 
-""" Plotting the Confusion Matrix
-       UNCOMMENT TO USE """
+# """ Plotting the Confusion Matrix
+#        UNCOMMENT TO USE """
 
-import matplotlib.pyplot as plt 
-import numpy as np
-#%matplotlib inline
-#<<%Matplotlib will not work in normal Sublime environment
-plt.imshow(c_m, interpolation='nearest', cmap=plt.cm.Wistia)
-classNames = ['Negative','Neutral','Positive']
-plt.title('SVM Confusion Matrix - Test Data')
-plt.ylabel('True label')
-plt.xlabel('Predicted label')
-tick_marks = np.arange(len(classNames))
-plt.xticks(tick_marks, classNames, rotation=45)
-plt.yticks(tick_marks, classNames)
+# import matplotlib.pyplot as plt 
+# import numpy as np
+# #%matplotlib inline
+# #<<%Matplotlib will not work in normal Sublime environment
+# plt.imshow(c_m, interpolation='nearest', cmap=plt.cm.Wistia)
+# classNames = ['Negative','Neutral','Positive']
+# plt.title('SVM Confusion Matrix - Test Data')
+# plt.ylabel('True label')
+# plt.xlabel('Predicted label')
+# tick_marks = np.arange(len(classNames))
+# plt.xticks(tick_marks, classNames, rotation=45)
+# plt.yticks(tick_marks, classNames)
 
 
-for i in range(3):
-    for j in range(3):
-        plt.text(j,i, str(c_m[i][j]))
-plt.show()          
+# for i in range(3):
+#     for j in range(3):
+#         plt.text(j,i, str(c_m[i][j]))
+# plt.show()          
 
 
 ################
@@ -207,6 +211,39 @@ plt.show()
 #######################################################################################################
 #Regression for Prediction of Bitcoin
 
+X_feat = Regress_df.drop(["Price"], axis=1)
+X_feat = X_feat.values
+Y_targ = Regress_df["Price"].values
+Y_targ = Y_targ.reshape(-1, 1)
+
+# X_feat = Regress_df.iloc[:,0:1]
+# Y_targ = Regress_df.iloc[:,2]
+# Y_targ= np.ravel(Y_targ)
+#Y_targ = Y_targ.reshape(-1, 1)
+
+sc = StandardScaler()
+
+X_feat = sc.fit_transform(X_feat)
+Y_targ = sc.fit_transform(Y_targ)
+
+
+#X_feat_train, X_feat_test, y_train, y_test = train_test_split(X_feat, Y_targ, test_size=0.2, random_state = 4)
+
+#Mutiple Linear Regression
+import statsmodels.api as sm
+X_Scaled1 = sm.add_constant(X_feat)
+model = sm.OLS(Y_targ, X_Scaled1)
+fitt = model.fit()
+print(fitt.summary())
+# y_train = y_train.reshape(-1, 1)
+# y_test = y_test.reshape(-1, 1)
+# RFR = ensemble.RandomForestRegressor(n_estimators = 400, random_state=4)
+
+# model = RFR.fit(X_feat_train,y_train)
+# Predict_y = RFR.predict(y_test)
+
+# print(metrics.accuracy_score(y_test, Predict_y))
+# print(metrics.mean_absolute_error(y_test, Predict_y))
 """  INCOMPLETE Neural Network Implementation of Regressio/Prediction Problem """
 
 # from keras.models import Sequential
